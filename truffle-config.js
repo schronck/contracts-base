3986551e675c758f0c636b1612a1aa28112d9f4a
Tomi_Ohl <ohlslager.tom@gmail.com>
2021-10-12 17:57:17 +0200

    Added more chains to truffle config and example dotenv

--- truffle-config.js
+++ truffle-config.js
@@ -30,34 +30,153 @@ module.exports = {
    *
    * $ truffle test --network <network-name>
    */
-  contracts_directory: "./contracts",
   networks: {
-    "ethereum.goerli": {
+    // Useful for testing. The `development` name is special - truffle uses it by default
+    // if it's defined here and no other network is specified at the command line.
+    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
+    // tab if you use this network and you must also set the `host`, `port` and `network_id`
+    // options below to some value.
+    development: {
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 8545, // Standard port (default: none)
+      network_id: "*" // Any network (default: none)
+    },
+    // One might need to adjust the gasPrice for some chains. The default value is 20 gwei.
+    ethereum: {
       provider: () =>
         new HDWalletProvider({
-          privateKeys: [process.env.PRIVATE_KEY],
-          providerOrUrl: `wss://goerli.infura.io/ws/v3/${process.env.INFURA_API_KEY}`,
+          mnemonic: process.env.MNEMONIC,
+          providerOrUrl: `wss://mainnet.infura.io/ws/v3/${process.env.INFURA_ID}`,
+          chainId: 1
+        }),
+      network_id: 1,
+      gas: 3500000,
+      confirmations: 2,
+      networkCheckTimeout: 9000000,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    goerli: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: process.env.MNEMONIC,
+          providerOrUrl: `wss://goerli.infura.io/ws/v3/${process.env.INFURA_ID}`,
           chainId: 5
         }),
       network_id: 5,
-      gas: 3000000, // Goerli has a lower block limit than mainnet
+      gas: 3500000,
+      confirmations: 1,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    kovan: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: process.env.MNEMONIC,
+          providerOrUrl: `wss://kovan.infura.io/ws/v3/${process.env.INFURA_ID}`,
+          chainId: 42
+        }),
+      network_id: 42,
+      gas: 3500000,
+      confirmations: 2,
+      networkCheckTimeout: 90000,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: process.env.MNEMONIC,
+          providerOrUrl: `wss://ropsten.infura.io/ws/v3/${process.env.INFURA_ID}`,
+          chainId: 3
+        }),
+      network_id: 3, // Ropsten's id
+      gas: 3500000, // Ropsten has a lower block limit than mainnet
       confirmations: 1, // # of confs to wait between deployments. (default: 0)
       networkCheckTimeout: 90000, // Seems like the default value was not enough
       timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true // Skip dry run before migrations? (default: false for public nets )
+    },
+    bsc: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: process.env.MNEMONIC,
+          providerOrUrl: `https://bsc-dataseed1.binance.org`,
+          chainId: 56
+        }),
+      network_id: 56,
+      gas: 3500000,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    bsctest: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: process.env.MNEMONIC,
+          providerOrUrl: `https://data-seed-prebsc-1-s1.binance.org:8545`,
+          chainId: 97
+        }),
+      network_id: 97,
+      gas: 3500000,
+      confirmations: 10,
+      networkCheckTimeout: 5000,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    polygon: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: process.env.MNEMONIC,
+          providerOrUrl: `wss://ws-matic-mainnet.chainstacklabs.com`,
+          chainId: 137
+        }),
+      network_id: 137,
+      gas: 3500000,
+      gasPrice: 5000000000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    mumbai: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: process.env.MNEMONIC,
+          providerOrUrl: `https://rpc-mumbai.matic.today`,
+          chainId: 80001
+        }),
+      network_id: 80001,
+      gas: 3500000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
     }
   },
 
-  // The `solc` compiler is set to optimize output bytecode with 200 runs, which is the standard these days
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    reporter: "eth-gas-reporter",
+    reporterOptions: {
+      showTimeSpent: true
+    }
+  },
+
+  // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.7",
-      settings: { optimizer: { enabled: true, runs: 200 } }
+      version: "0.8.7", // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      settings: {
+        // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+        //  evmVersion: "byzantium"
+      }
     }
   },
 
-  // This plugin allows to verify the source code of your contracts on Etherscan with this command:
-  // ETHERSCAN_API_KEY=<your_etherscan_api_key> truffle run verify <contract_name> --network <network_name>
   plugins: ["truffle-plugin-verify"],
 
   // This is just for the `truffle-plugin-verify` to catch the API key
