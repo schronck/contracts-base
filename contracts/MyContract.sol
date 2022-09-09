f12969ffb319573c90303cd7897125caf6f371ef
Tomi_Ohl <ohlslager.tom@gmail.com>
2022-09-09 17:12:16 +0200

    Add tool for testing custom errors Update tests

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -2,7 +2,13 @@
 pragma solidity 0.8.17;
 
 contract MyContract {
+    error Nope(address sender);
+
     function hello() external pure returns (string memory) {
         return "Hello friend";
+    }
+
+    function notHello() external view {
+        revert Nope(msg.sender);
     }
 }
--- test/MyContractTest.js
+++ test/MyContractTest.js
@@ -1,20 +1,27 @@
 const ethers = require("ethers");
 const { BN, expectRevert } = require("@openzeppelin/test-helpers");
+const { expectRevertCustomError } = require("custom-error-test-helper");
 const expect = require("chai").expect;
 
-const contractArtifact = artifacts.require("MyContract");
+const MyContract = artifacts.require("MyContract");
 
 contract("MyContract", async function (accounts) {
   let contract;
 
   beforeEach("deploy a new contract", async function () {
-    contract = await contractArtifact.new();
+    contract = await MyContract.new();
   });
 
   context("some context", async function () {
-    it("should return the correct string", async function () {
+    it("should greet correctly", async function () {
       const result = await contract.hello();
       expect(result).to.equal("Hello friend");
+    });
+
+    it("should not greet as well", async function () {
+      await expectRevertCustomError(MyContract, contract.notHello(), "Nope", [
+        accounts[0]
+      ]);
     });
   });
 });
